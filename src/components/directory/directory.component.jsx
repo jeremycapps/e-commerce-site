8d7852e44aa807bdb96d0c05eb850dee2e37bf4a
Jeremy Capps <jeremycapps@Jeremys-MBP-2.home>
2019-08-07 15:45:23 -0400

    add withRouter to menu item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,7 +13,7 @@ export default class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
@@ -47,12 +47,10 @@ export default class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
           <MenuItem 
             key={id} 
-            title={title}
-            imageUrl={imageUrl} 
-            size={size} />
+            {...otherSectionProps} />
         ))}
       </div>
     )
